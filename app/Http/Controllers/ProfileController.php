83b379bf465371620e2982f5ff70975825010d3d
Dami Oyediran <oyediran.viktor@gmail.com>
2021-11-10 23:13:05 +0100

    complete task 1

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\CreateProfileAction;
+use App\Http\Requests\ProfileCreateRequest;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function create(ProfileCreateRequest $request)
     {
-        $call = (new CreateProfileAction) -> call($request);
-        if($call){
+        $call = (new CreateProfileAction)->call($request);
+        if ($call) {
             return response()->json([], 200);
         }
         return response([], 500);
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->morphMany('App\Models\Category', 'categorable');
+    }
 }
